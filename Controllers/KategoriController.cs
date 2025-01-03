a4cc8cb7dfd0be68f872690a382d0daa09ba0be4
MRSoft <88499726+MRSoft2211@users.noreply.github.com>
2025-01-04 02:53:50 +0300

    Add project files.

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            //var degerler = db.TBLKATEGORILER.ToList();
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 4);
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();  
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();  
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var kategori=db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id) 
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("Kategorigetir",ktgr);
+        }
+        public ActionResult KategoriGuncelle(TBLKATEGORILER p1)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktgr.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
